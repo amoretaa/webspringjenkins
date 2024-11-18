a00843468bc1c4db935681faec174f880bdbbadb
amoretabilbo <amoreta@bilbomatica.es>
2024-11-18 16:16:07 +0100

    Junit

--- webspring/src/test/java/com/arquitecturajava/webspring/WebspringApplicationTests.java
+++ webspring/src/test/java/com/arquitecturajava/webspring/WebspringApplicationTests.java
@@ -1,13 +1,25 @@
 package com.arquitecturajava.webspring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.arquitecturajava.webspring.models.Persona;
 
 @SpringBootTest
 class WebspringApplicationTests {
-
+	
+	@Autowired
+	PersonaRepository personaRepositorio; 
+	
 	@Test
-	void contextLoads() {
+	void addPersonaRepositorioTest() {
+		
+		personaRepositorio.add(new Persona("juan"));
+		assertEquals(personaRepositorio.buscarTodos().size(),2);
+	
 	}
 
 }
